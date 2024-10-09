9310f556ed5706fa7f311ff795d9f9619263b9ad
Anmol Varshney <varsh@Anmols-Macbook-1119.local>
2024-10-09 14:42:12 -0700

    Added questions, added a foot note, added a dialog box for correct and incorrect answer

--- lib/practice_questions_page.dart
+++ lib/practice_questions_page.dart
@@ -10,6 +10,7 @@ class PracticeQuestionsPage extends StatefulWidget {
 
 class _PracticeQuestionsPageState extends State<PracticeQuestionsPage> {
   int currentIndex = 0;
+  String? selectedAnswer;
 
   // List of questions for the user to practice
   List<Question> questions = [
@@ -31,7 +32,18 @@ class _PracticeQuestionsPageState extends State<PracticeQuestionsPage> {
       options: ['104', '80', '120', '55.5'],
       correctAnswer: '104',
     ),
-    // Add more questions here
+    Question(
+      question: "4. Calculate the area of the rectangle.",
+      imageUrl: "assets/images/rectangle_q4.png",
+      options: ['84 in', '70 in', '91 in', '130 in'],
+      correctAnswer: '91 in',
+    ),
+    Question(
+      question: "5. Calculate the area of the rectangle.",
+      imageUrl: "assets/images/rectangle_q5.png",
+      options: ['30 in', '26.25 in', '24.5 in', '21 in'],
+      correctAnswer: '26.25 in',
+    ),
   ];
 
   @override
@@ -42,7 +54,7 @@ class _PracticeQuestionsPageState extends State<PracticeQuestionsPage> {
       appBar: AppBar(
         title: const Text("Practice Section - Area -> Rectangle"),
         leading: IconButton(
-          icon: const Icon(Icons.home), // Home button on top-left
+          icon: const Icon(Icons.home),
           onPressed: () {
             Navigator.of(context).pop();
           },
@@ -52,38 +64,41 @@ class _PracticeQuestionsPageState extends State<PracticeQuestionsPage> {
         children: <Widget>[
           Column(
             children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Text(question.question,
-                    style: const TextStyle(fontSize: 20)),
-              ),
-              SizedBox(
-                width: 400, // Set desired width
-                height: 200, // Set desired height
-                child: Image.asset(question.imageUrl),
-              ),
-              const SizedBox(height: 20), // Add spacing before the buttons
-
-              // Display two buttons in a row
-              for (int i = 0; i < question.options.length; i += 2)
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 8.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      _buildButton(
-                          context, question.options[i]), // First button
-                      if (i + 1 < question.options.length)
-                        _buildButton(
-                            context,
-                            question
-                                .options[i + 1]), // Second button if it exists
-                    ],
-                  ),
+              Expanded(
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(
+                        question.question,
+                        style: const TextStyle(fontSize: 20),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 400,
+                      height: 200,
+                      child: Image.asset(question.imageUrl),
+                    ),
+                    const SizedBox(height: 20),
+                    // Display two buttons in a row
+                    for (int i = 0; i < question.options.length; i += 2)
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            _buildButton(context, question.options[i]),
+                            if (i + 1 < question.options.length)
+                              _buildButton(context, question.options[i + 1]),
+                          ],
+                        ),
+                      ),
+                  ],
                 ),
+              ),
+              _buildInfoSection(),
             ],
           ),
-
           // Left navigation arrow
           Positioned(
             left: 10,
@@ -96,12 +111,12 @@ class _PracticeQuestionsPageState extends State<PracticeQuestionsPage> {
                   ? () {
                       setState(() {
                         currentIndex--;
+                        selectedAnswer = null; // Reset selected answer
                       });
                     }
-                  : null, // Disable if at the first question
+                  : null,
             ),
           ),
-
           // Right navigation arrow
           Positioned(
             right: 10,
@@ -114,9 +129,10 @@ class _PracticeQuestionsPageState extends State<PracticeQuestionsPage> {
                   ? () {
                       setState(() {
                         currentIndex++;
+                        selectedAnswer = null; // Reset selected answer
                       });
                     }
-                  : null, // Disable if at the last question
+                  : null,
             ),
           ),
         ],
@@ -125,21 +141,43 @@ class _PracticeQuestionsPageState extends State<PracticeQuestionsPage> {
   }
 
   Widget _buildButton(BuildContext context, String option) {
+    Color buttonColor;
+    if (selectedAnswer == null) {
+      buttonColor = const Color.fromARGB(255, 23, 127, 237); // Default color
+    } else if (selectedAnswer == option &&
+        selectedAnswer == questions[currentIndex].correctAnswer) {
+      buttonColor = Colors.green; // Highlight correct answer in green
+    } else if (selectedAnswer == option) {
+      buttonColor = Colors.red; // Highlight selected incorrect answer in red
+    } else if (option == questions[currentIndex].correctAnswer) {
+      buttonColor = Colors
+          .green; // Highlight correct answer in green when incorrect is selected
+    } else {
+      buttonColor = const Color.fromARGB(255, 23, 127, 237);
+    }
+
     return Flexible(
       child: SizedBox(
-        width: 150, // Set desired button width
-        height: 50, // Set desired button height
+        width: 150,
+        height: 50,
         child: ElevatedButton(
-          onPressed: () => checkAnswer(option),
+          onPressed: selectedAnswer == null
+              ? () {
+                  setState(() {
+                    selectedAnswer = option;
+                  });
+                  checkAnswer(option);
+                }
+              : null,
           style: ElevatedButton.styleFrom(
-            backgroundColor: const Color.fromARGB(255, 23, 127, 237),
+            backgroundColor: buttonColor,
           ),
           child: Text(
             option,
             style: const TextStyle(
-              color: Colors.white, // Set the text color to white
-              fontWeight: FontWeight.bold, // Make the text bold
-              fontSize: 18, // Optionally, increase the font size
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 18,
             ),
           ),
         ),
@@ -147,29 +185,52 @@ class _PracticeQuestionsPageState extends State<PracticeQuestionsPage> {
     );
   }
 
-  void checkAnswer(String selectedOption) {
-    if (selectedOption == questions[currentIndex].correctAnswer) {
-      showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: const Text("Correct Answer"),
-            content: Image.asset("assets/images/Q1_correct_answer_image.png"),
-            actions: <Widget>[
-              TextButton(
-                child: const Text("Close"),
-                onPressed: () {
-                  Navigator.of(context).pop(); // Close the dialog
-                },
-              ),
-            ],
-          );
-        },
-      );
-    } else {
-      // Handle incorrect answer
-    }
+  Widget _buildInfoSection() {
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.all(16.0),
+      color: Colors.grey[200],
+      child: const Text(
+        "Note: A ft is a foot (plural: feet), cm is centimeter, mm is millimeter, and 'in' is Inches",
+        style: TextStyle(
+          fontSize: 16,
+          color: Colors.black87,
+        ),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
 
-    // Handle quiz completion or navigation to next question
+  void checkAnswer(String selectedOption) {
+    bool isCorrect = selectedOption == questions[currentIndex].correctAnswer;
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(isCorrect ? "Correct Answer" : "Incorrect Answer"),
+          content: Text(
+            isCorrect
+                ? "Good job! You selected the correct answer."
+                : "Oops! The correct answer is ${questions[currentIndex].correctAnswer}.",
+            textAlign: TextAlign.center,
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text("Close"),
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+                if (isCorrect && currentIndex < questions.length - 1) {
+                  setState(() {
+                    currentIndex++;
+                    selectedAnswer = null; // Reset for the next question
+                  });
+                }
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 }
